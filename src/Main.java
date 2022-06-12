d6bd87e1b7aac482fa76f0e9bb63cfb13ff50fa8
hanawinchester <hana.rose.winchester@gmail.com>
2022-06-12 13:04:53 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("What is your age? ");
+        int age = 21;
+
+        if (age >= 21) {
+            System.out.println("You get a wrist band!");
+        }
+    }
+}
